d3e6748003ac48f232090f53dde4bafe176f1330
SlickJohnson <liwa.johnson@gmail.com>
2018-01-15 17:24:39 -0800

    Implement binary search

--- challenges/search.py
+++ challenges/search.py
@@ -22,41 +22,47 @@ def linear_search_recursive(array, item, index=0):
     if index >= len(array):
         return None
 
-    return index if array[index] == item else linear_search_recursive(array, item, index + 1)
+    if item == array[index]:
+        return index
+
+    return linear_search_recursive(array, item, index + 1)
 
 
 def binary_search(array, item):
-    """return the index of item in sorted array or None if item is not found"""
-    # implement binary_search_iterative and binary_search_recursive below, then
-    # change this to call your implementation to verify it passes all tests
+    """Return the index of item in sorted array or None if item is not found."""
 
     return binary_search_iterative(array, item)
     # return binary_search_recursive(array, item)
 
 
 def binary_search_iterative(array, item):
-    index = len(array) // 2 - 1
+    left = 0
+    right = len(array) - 1
 
-    while index >= 1:
-        print(index)
-        if array[index] == item:
-                return index
+    while left <= right:
+        index = (left + right) // 2
 
-        if item > array[index]:
-            index += index // 2
+        if item == array[index]:
+            return index
+
+        if item < array[index]:
+            right = index - 1
         else:
-            index -= index // 2
+            left = index + 1
 
-
-
-    # TODO: implement binary search iteratively here
-
-    # once implemented, change binary_search to call binary_search_iterative
-    # to verify that your iterative implementation passes all tests
+    return None
 
 
 def binary_search_recursive(array, item, left=None, right=None):
-    # TODO: implement binary search recursively here
-    pass
-    # once implemented, change binary_search to call binary_search_recursive
-    # to verify that your recursive implementation passes all tests
+    if left > right:
+        return None
+
+    index = (left + right) // 2
+
+    if item == array[index]:
+        return index
+
+    if item < array[index]:
+        return binary_search_recursive(array, item, left, index - 1)
+    else:
+        return binary_search_recursive(array, item, index + 1, right)

297a6f06cf34b321ab72f3a54b0ed3cc9651ecf5
SlickJohnson <liwa.johnson@gmail.com>
2018-01-16 16:08:00 -0800

    Implement iterative palindrome check

---
+++ challenges/palindromes.py
@@ -0,0 +1,80 @@
+#!python
+
+import string
+# Hint: Use these string constants to ignore capitalization and/or punctuation
+# string.ascii_lowercase is 'abcdefghijklmnopqrstuvwxyz'
+# string.ascii_uppercase is 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# string.ascii_letters is ascii_lowercase + ascii_uppercase
+
+
+def is_palindrome(text):
+    """A string of characters is a palindrome if it reads the same forwards and
+    backwards, ignoring punctuation, whitespace, and letter casing."""
+    # implement is_palindrome_iterative and is_palindrome_recursive below, then
+    # change this to call your implementation to verify it passes all tests
+    assert isinstance(text, str), 'input is not a string: {}'.format(text)
+    return is_palindrome_iterative(text)
+    # return is_palindrome_recursive(text)
+
+
+def is_palindrome_iterative(text):
+    """Check if provided text is a palindrome iteratively.
+
+    Args:
+        text: str -- the string that will be checked for palindromness.
+
+    Returns: bool -- states whether or not the input is a palindrome.
+
+    """
+    # ignore case, punctuation, and whitespace.
+    translator = str.maketrans("", "", string.punctuation)
+    text = text.lower().translate(translator).replace(" ", "")
+
+    # Only one character so it's a palindrome.
+    if len(text) == 1:
+        return True
+
+    # rearange character order.
+    palindrome = []
+    for char in text:
+        palindrome.insert(0, char)
+
+    return "".join(palindrome) == text
+
+
+def is_palindrome_recursive(text, left=None, right=None):
+    """Check if provided text is a palindrome recusrsively.
+
+    Args:
+        text: str -- the string that will be checked for palindromness.
+        left: ?? --
+        right: ?? --
+
+    Returns: bool -- states whether or not the input is a palindrome.
+
+    """
+    stripped_text = text.strip().lower()
+
+    # Only one character so it's a palindrome.
+    if len(stripped_text) == 1:
+        return True
+
+
+
+
+def main():
+    import sys
+    args = sys.argv[1:]  # Ignore script file name
+    if len(args) > 0:
+        for arg in args:
+            is_pal = is_palindrome(arg)
+            result = 'PASS' if is_pal else 'FAIL'
+            is_str = 'is' if is_pal else 'is not'
+            print('{}: {} {} a palindrome'.format(result, repr(arg), is_str))
+    else:
+        print('Usage: {} string1 string2 ... stringN'.format(sys.argv[0]))
+        print('  checks if each argument given is a palindrome')
+
+
+if __name__ == '__main__':
+    main()

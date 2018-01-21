a76fb611b2d1b190aa83a1cd10d39f5836fef626
SlickJohnson <liwa.johnson@gmail.com>
2018-01-21 13:48:38 -0800

    Implement stack data structure using linkedlists

--- source/stack.py
+++ source/stack.py
@@ -1,14 +1,23 @@
 #!python
-
+"""implementations of stack data structures."""
 from linkedlist import LinkedList
 
 
-# Implement LinkedStack below, then change the assignment at the bottom
-# to use this Stack implementation to verify it passes all tests
 class LinkedStack(object):
+    """LinkedList stack implementation.
+
+    Attributes:
+        list: LinkedList -- stores the items
+
+    """
 
     def __init__(self, iterable=None):
-        """Initialize this stack and push the given items, if any."""
+        """Initialize this stack and push the given items, if any.
+
+        Args:
+            iteratable: iterable -- An optional iterable to populate the stack
+                with.
+        """
         # Initialize a new linked list to store the items
         self.list = LinkedList()
         if iterable is not None:
@@ -21,28 +30,49 @@ class LinkedStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this stack."""
-        # TODO: Count number of items
+        return self.list.length()
 
     def push(self, item):
         """Insert the given item on the top of this stack.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Push given item
+
+        Args:
+            item: anytype -- data that will be added to the stack.
+        """
+        self.list.prepend(item)  # O(1)
 
     def peek(self):
-        """Return the item on the top of this stack without removing it,
-        or None if this stack is empty."""
-        # TODO: Return top item, if any
+        """Return the item on the top of this stack."""
+        peek = self.list.head  # O(1)
+        if peek:  # O(1)
+            return peek.data  # O(1)
+
+        # Stack is empty, return None.
+        return None  # O(1)
 
     def pop(self):
-        """Remove and return the item on the top of this stack,
-        or raise ValueError if this stack is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+        """Remove and return the item on the top of this stack.
 
+        Performance:
+            Best/Worst: O(1), because pop only affects the head of the
+                linkedlist.
+
+        Returns: anytype -- returns the item that sits at the top of the stack.
+
+        Raises:
+            ValueError: if this stack is empty.
+
+        """
+        if self.is_empty():
+            raise ValueError('Empty stack')
+
+        pop = self.list.head.data  # O(1)
+        self.list.delete(pop)  # O(1)
+
+        return pop  # O(1)
 
 # Implement ArrayStack below, then change the assignment at the bottom
 # to use this Stack implementation to verify it passes all tests

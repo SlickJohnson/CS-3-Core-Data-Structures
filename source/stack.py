686c54e1aab3225b3a352a46ca6b2861848e9704
SlickJohnson <liwa.johnson@gmail.com>
2018-01-21 14:10:25 -0800

    Implement stack data structure using python list

--- source/stack.py
+++ source/stack.py
@@ -15,8 +15,7 @@ class LinkedStack(object):
         """Initialize this stack and push the given items, if any.
 
         Args:
-            iteratable: iterable -- An optional iterable to populate the stack
-                with.
+            iteratable: iterable -- Items to populate the stack with.
         """
         # Initialize a new linked list to store the items
         self.list = LinkedList()
@@ -74,12 +73,21 @@ class LinkedStack(object):
 
         return pop  # O(1)
 
-# Implement ArrayStack below, then change the assignment at the bottom
-# to use this Stack implementation to verify it passes all tests
+
 class ArrayStack(object):
+    """LinkedList stack implementation.
+
+    Attributes:
+        list: list -- stores the items
+
+    """
 
     def __init__(self, iterable=None):
-        """Initialize this stack and push the given items, if any."""
+        """Initialize this stack and push the given items, if any.
+
+        Args:
+            iteratable: iterable -- Items to populate the stack with.
+        """
         # Initialize a new list (dynamic array) to store the items
         self.list = list()
         if iterable is not None:
@@ -92,30 +100,48 @@ class ArrayStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        return self.length() == 0  # O(1)
 
     def length(self):
         """Return the number of items in this stack."""
-        # TODO: Count number of items
+        return len(self.list)
 
     def push(self, item):
         """Insert the given item on the top of this stack.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+
+        Args:
+            item: anytype -- data that will be added to the stack.
+
+        Performance:
+            Best/Worst: O(1), adding to the end of an list is O(1)
+        """
+        self.list.append(item)  # O(1)
 
     def peek(self):
-        """Return the item on the top of this stack without removing it,
-        or None if this stack is empty."""
-        # TODO: Return top item, if any
+        """Return the item on the top of this stack without removing it."""
+        # Return None if stack is empty.
+        if self.is_empty():
+            return None
+
+        return self.list[-1]  # O(1)
 
     def pop(self):
-        """Remove and return the item on the top of this stack,
-        or raise ValueError if this stack is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+        """Remove and return the item on the top of this stack.
+
+        Performance:
+            Best/Worst: O(1), only concerned with the last entry in the list.
+
+        Raises:
+            ValueError: if stack is empty.
+
+        """
+        if self.is_empty():
+            raise ValueError('Empty stack')
+
+        return self.list.pop()  # O(1)
 
 
 # Implement LinkedStack and ArrayStack above, then change the assignment below
 # to use each of your Stack implementations to verify they each pass all tests
-Stack = LinkedStack
-# Stack = ArrayStack
+# Stack = LinkedStack
+Stack = ArrayStack

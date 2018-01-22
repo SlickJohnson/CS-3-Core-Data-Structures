95db7db1896c0b95ae83bc9f0afb277561698dfc
SlickJohnson <liwa.johnson@gmail.com>
2018-01-22 12:21:34 -0800

    Implement queue data structures

--- source/queue.py
+++ source/queue.py
@@ -1,14 +1,22 @@
 #!python
-
+"""Implementation of Queue data structure."""
 from linkedlist import LinkedList
 
 
-# Implement LinkedQueue below, then change the assignment at the bottom
-# to use this Queue implementation to verify it passes all tests
 class LinkedQueue(object):
+    """Queue implemented using a linkedlist.
+
+    Attributes:
+        list: LinkedList -- stores the queue.
+
+    """
 
     def __init__(self, iterable=None):
-        """Initialize this queue and enqueue the given items, if any."""
+        """Initialize this queue and enqueue the given items, if any.
+
+        Args:
+            iterable: iterable -- items to put in the queue.
+        """
         # Initialize a new linked list to store the items
         self.list = LinkedList()
         if iterable is not None:
@@ -21,35 +29,76 @@ class LinkedQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        return self.list.length()
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+
+        Args:
+            item: anytype -- what to put in the queue.
+
+        Performance:
+            Best/Worst: O(1), linkedlist appending requires only a few
+                operations.
+
+        """
+        self.list.append(item)  # O(1)
 
     def front(self):
-        """Return the item at the front of this queue without removing it,
-        or None if this queue is empty."""
-        # TODO: Return front item, if any
+        """Return the item at the front of this queue without removing it.
+
+        Returns:
+            front: anytype -- the first item in the queue.
+
+        Performance:
+            Best/Worst: O(1), the front can be accessed through the variable
+                'head' in the linkedlist.
+
+        """
+        front = self.list.head  # O(1)
+        if front:  # O(1)
+            return front.data  # O(1)
+
+        # Queue is empty, return None.
+        return None  # O(1)
 
     def dequeue(self):
-        """Remove and return the item at the front of this queue,
-        or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return front item, if any
+        """Remove and return the item at the front of this queue.
+
+        Returns:
+            front: anytype -- the first item in the queue.
+
+        Raises:
+            ValueError: If queue is empty.
+
+        """
+        if self.is_empty():
+            raise ValueError('Empty queue')
+
+        front = self.list.head.data  # O(1)
+        self.list.delete(front)  # O(1)
+
+        return front  # O(1)
 
 
-# Implement ArrayQueue below, then change the assignment at the bottom
-# to use this Queue implementation to verify it passes all tests
 class ArrayQueue(object):
+    """Queue implemented using an array.
+
+    Attributes:
+        list: list -- stores the queue.
+
+    """
 
     def __init__(self, iterable=None):
-        """Initialize this queue and enqueue the given items, if any."""
+        """Initialize this queue and enqueue the given items, if any.
+
+        Args:
+            iterable: iterable -- items to put in the queue.
+        """
         # Initialize a new list (dynamic array) to store the items
         self.list = list()
         if iterable is not None:
@@ -62,30 +111,62 @@ class ArrayQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        return self.length() == 0
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        return len(self.list)
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+
+        Args:
+            item: anytype -- what to put in the queue.
+
+        Performance:
+            Best/Worst: O(1), linkedlist appending requires only a few
+                operations.
+
+        """
+        self.list.append(item)
 
     def front(self):
-        """Return the item at the front of this queue without removing it,
-        or None if this queue is empty."""
-        # TODO: Return front item, if any
+        """Return the item at the front of this queue without removing it.
+
+        Returns:
+            front: anytype -- the first item in the queue.
+
+        Performance:
+            Best/Worst: O(1), the front can be accessed through the variable
+                'head' in the linkedlist.
+
+        """
+        # Return None if stack is empty.
+        if self.is_empty():  # O(1)
+            return None  # O(1)
+
+        return self.list[0]  # O(1)
 
     def dequeue(self):
-        """Remove and return the item at the front of this queue,
-        or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return front item, if any
+        """Remove and return the item at the front of this queue.
+
+        Returns:
+            front: anytype -- the first item in the queue.
+
+        Raises:
+            ValueError: If queue is empty.
+
+        """
+        if self.is_empty():  # O(1)
+            raise ValueError('Empty queue')  # O(1)
+
+        front = self.front()  # O(1)
+        self.list.remove(front)  # O(n)
+
+        return front
 
 
 # Implement LinkedQueue and ArrayQueue above, then change the assignment below
 # to use each of your Queue implementations to verify they each pass all tests
-Queue = LinkedQueue
+Queue = ArrayQueue
 # Queue = ArrayQueue

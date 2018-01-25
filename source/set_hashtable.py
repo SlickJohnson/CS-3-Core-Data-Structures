c99c09512bca295463b36431e7a2ccbe3d9402e7
SlickJohnson <liwa.johnson@gmail.com>
2018-01-25 10:52:26 -0800

    Outline Set class and tests

---
+++ source/set_hashtable.py
@@ -0,0 +1,104 @@
+#!python
+"""Implementation of the Set data type."""
+from hashtable import HashTable
+
+
+class Set(HashTable):
+    """A Set data structure implemented using a HashTable."""
+
+    def __init__(self, elements=None):
+        """Initialize a new empty set, and add any given elements."""
+        super().__init__()
+
+    def contains(self, element):
+        """Check whether element is in this set.
+
+        Args:
+            element: any -- what to check for in the set.
+
+        Complexity:
+            Best:
+            Worst:
+
+        Returns:
+            bool: whether the element is in this set.
+
+        """
+        pass
+
+    def add(self, element):
+        """Add element to this set, if not present already.
+
+        Args:
+            element: any -- what to add to this set.
+
+        Complexity:
+            Best:
+            Worst:
+
+        """
+        pass
+
+    def remove(self, element):
+        """Remove element from this set, if present.
+
+        Args:
+            element: any -- the element to remove from this set.
+
+        Complexity:
+            Best:
+            Worst:
+
+        Raises:
+            KeyError: if the element is not in this set.
+
+        """
+        pass
+
+    def union(self, other_set):
+        """Return a new set that is the union of this set and other_set.
+
+        Args:
+            other_set: Set -- the set that will be combined with this set.
+
+        Complexity:
+            Best:
+            Worst:
+
+        Returns:
+            Set: the union of the two sets.
+
+        """
+        pass
+
+    def intersection(self, other_set):
+        """Return a new set that is the intersection of this set and other_set.
+
+        Args:
+            other_set: Set -- the set that will intersect this set.
+
+        Complexity:
+            Best:
+            Worst:
+
+        Returns:
+            Set: the intersection between the two sets.
+
+        """
+        pass
+
+    def difference(self, other_set):
+        """Return a new set that is the difference of this set and other_set.
+
+        Args:
+            other_set: Set -- the set that will be compared to this set.
+
+        Complexity:
+            Best:
+            Worst:
+
+        Returns:
+            Set: the difference between the two sets.
+
+        """
+        pass
---
+++ source/set_test.py
@@ -0,0 +1,44 @@
+#!python
+"""Tests for Set data stuctures."""
+from set_hashtable import Set
+import unittest
+
+
+class HashTableSetTest(unittest.TestCase):
+    """Test Set implemented using a HashTable."""
+
+    def test_init(self):
+        """Initialize Set and check to see if default values are set."""
+        pass
+
+    def test_items(self):
+        """Check if a list of items in the Set can be retrieved."""
+        pass
+
+    def test_length(self):
+        """Check if the length is updated properly."""
+        pass
+
+    def test_contains(self):
+        """Check to see if the contains method works as intended."""
+        pass
+
+    def test_add_and_get(self):
+        """Check if adding and retrieving items works as intended."""
+        pass
+
+    def test_remove(self):
+        """Check to see if items can be removed from the Set."""
+        pass
+
+    def test_union(self):
+        """Check if the union operation works as intended."""
+        pass
+
+    def test_intersection(self):
+        """Check if the intersection operation works as intended."""
+        pass
+
+    def test_difference(self):
+        """Check if the difference operation works as intended."""
+        pass

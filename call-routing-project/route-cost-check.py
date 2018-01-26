4249b1325446731e90f84c0ef99c5ad421d453f4
SlickJohnson <liwa.johnson@gmail.com>
2018-01-25 18:19:50 -0800

    Outline call routing project

---
+++ call-routing-project/route-cost-check.py
@@ -0,0 +1,59 @@
+"""Module to help check the cost for calling a number."""
+
+
+def get_route_cost(number, routes):
+    """Return the cost for calling the given number.
+
+    Args:
+        number: str -- the number who's cost will be found.
+        routes: list -- a list with all the carrier routes and costs.
+
+    Complexity:
+        Best:
+        Worst:
+
+    Returns:
+        str: the cost for calling the number.
+
+    """
+    pass
+
+
+def __run_scenario_1():
+    """Check cost one time."""
+    print("running scenario 1...")
+
+
+def __run_scenario_2():
+    """Check cost for a list of numbers."""
+    print("Not yet completed...")
+
+
+def __run_scenario_3():
+    """Check cost with multiple long carrier route lists."""
+    print("Not yet completed...")
+
+
+def __run_scenario_4():
+    """High-throughput pricing API."""
+    print("Not yet completed...")
+
+
+def __run_scenario_5():
+    """Check cost with Changing carrier route lists."""
+    print("Not yet completed...")
+
+
+if __name__ == "__main__":
+    import sys
+    args = sys.argv
+    if len(args) > 1:
+        # determines what scenario is going to run.
+        scenario_number = int(args[1])
+        scnarios = {
+            1: __run_scenario_1,
+            2: __run_scenario_2,
+            3: __run_scenario_3,
+            4: __run_scenario_4,
+            5: __run_scenario_5
+        }.get(scenario_number)()
